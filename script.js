97a89c7832cfa4fe1784958d6498e4f43bcfd863
Yoisell Cervantes Almaguer <46346959+ycervantes1985@users.noreply.github.com>
2022-09-22 11:43:34 -0300

    [TAREA] sensei

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ class ninja{
     velocidad;
     fuerza;
 
-    constructor (nombre, salud, velocidad, fuerza){
+    constructor (nombre){
     this.nombre = nombre;
-    this.salud = salud;
-    this.velocidad = velocidad;
-    this.fuerza = fuerza;
+    this.salud = 100;
+    this.velocidad = 3;
+    this.fuerza = 3;
 
     }
 
@@ -18,7 +18,7 @@ class ninja{
     }
 
     showStats(){
-    console.log(`El nombre es ${this.nombre}, la fuerza es ${this.fuerza}, la velocidad es ${this.velocidad} y la salud es ${this.salud}`);
+    console.log(`Nombre:${this.nombre} Fuerza:${this.fuerza} Velocidad:${this.velocidad} Salud:${this.salud}`);
     }
 
     drinkSake(){
@@ -31,7 +31,32 @@ class ninja{
 }
 
 
-const ninja2 = new ninja("yoisell", 10, 3, 3)
+const ninja2 = new ninja("yoisell")
 console.log(ninja2);
 ninja2.sayName();
 ninja2.drinkSake();
+
+
+class Sensei extends ninja{
+
+constructor(nombre){
+    super();
+this.nombre = nombre;
+this.salud = 200;
+this.velocidad = 10;
+this.fuerza = 10;
+this.sabiduria = 10;
+
+}
+speakWisdom(){
+console.log("Lo que un programador puede hacer e un mes, dos programadores pueden hacerlo en dos meses")
+this.drinkSake();
+}
+
+}
+
+
+const sensei1 = new Sensei("yakimeshi")
+//console.log(sensei1);
+sensei1.speakWisdom();
+sensei1.showStats();
